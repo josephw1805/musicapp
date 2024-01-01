33d6d4b2ad3b01db529ab1738a177ca04cf316c7
josephW1805 <joseph.weng.1805@gmail.com>
2024-01-01 11:27:49 -0500

    final updates

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://musicapp-server.vercel.app/api",
 });
 
 export default Axios;
