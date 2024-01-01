ea19b0b332d8ed2a5050cb3d64227578ea6f4462
josephW1805 <joseph.weng.1805@gmail.com>
2024-01-01 11:24:34 -0500

    updates profile

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "https://musicapp-server.vercel.app/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 export default Axios;
--- server/config/firebaseStorage.js
+++ server/config/firebaseStorage.js
@@ -1,12 +1,19 @@
 import { getStorage } from "firebase-admin/storage";
-import { initializeApp } from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 initializeApp({
-  project_id: process.env.PROJECT_ID,
-  app_id: process.env.APP_ID,
+  credential: cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  }),
   storageBucket: process.env.STORAGE_BUCKET,
 });
 
